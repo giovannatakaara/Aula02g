634e8280d565cd6aea59cce8799d2813ddcac535
giovannatakaara <gkptakaara@minha.fag.edu.br>
2025-08-13 18:14:10 -0300

    Feito Ola-Mundo

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        olaMundo();
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
     }
 }
